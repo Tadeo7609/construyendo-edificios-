108c6b8f1079cbb788d017ba39f402051b3c63b7
Tadeo7609 <150203754+Tadeo7609@users.noreply.github.com>
2023-12-13 18:36:47 -0600

    Add files via upload

---
+++ building.js
@@ -0,0 +1,16 @@
+
+class Building{
+    constructor(x,y,h)
+  {
+    this.x = x;
+    this.y = y;   
+    this.w = 25;
+    this.h = h;
+    
+  }  
+  display()
+  {
+   rect(this.x,this.y, this.w, this.h); 
+  }  
+  
+}
